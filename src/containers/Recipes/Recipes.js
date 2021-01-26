d52f95f48b96df5de2b63a524c76b7d067c33310
Andres Ramos <andresramos@Andress-MacBook-Pro.local>
2021-01-26 18:36:22 +0100

    get request so fetch all the recipes from firebase done

--- src/containers/Recipes/Recipes.js
+++ src/containers/Recipes/Recipes.js
@@ -1,48 +1,59 @@
 // Use Aux to wrap all the recipes
 
 import React, {Component} from 'react';
+
+
 import Recipe from './Recipe/Recipe';
 import classes from './Recipes.css';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import axios from '../../axios-orders';
 
 class Recipes extends Component {
 
 
     state = {
-        recipes : [{
-            title : "Recipe1",
-            description : "gfd"
-        },
-        {
-            title : "Recipe2",
-            description : "asf"
-        }, 
-        {
-            title : "Recipe3",
-            description : "gfd"
-        }, 
-        {
-            title : "Recipe4",
-            description : "asf"
-        }, 
-        {
-            title : "Recipe5",
-            description : "gfd"
-        }, 
-        {
-            title : "Recipe6",
-            description : "asf"
-        }]
+        recipes : null
 
     };
 
+
+    componentDidMount() {
+        console.log("hi");
+        axios.get("/recipes.json")
+            .then(response =>{
+                console.log(response.data);
+                const newRecipes = [];
+                let newRecipe = null;
+                for (let recipeData in response.data) {
+                    newRecipe = [...response.data[recipeData]];
+                    console.log(response.data[recipeData].title);
+                    console.log(response.data[recipeData].description);
+                    newRecipe = ({
+                        title : response.data[recipeData].title,
+                        description: response.data[recipeData].description
+                    });
+
+                    newRecipes.push(newRecipe);
+                }
+                this.setState({recipes : newRecipes});
+                console.log(this.state.recipes);
+            }).catch(error => {
+                console.log(error);
+            });
+    }
+
     render () {
 
-        const recipesToJSX = this.state.recipes.map((recipe,index) => {
-            return <Recipe key = {index} title = {recipe.title} description = {recipe.description} />
+        let recipesToJSX = <Spinner/>;
 
+        if (this.state.recipes) {
+            recipesToJSX = this.state.recipes.map((recipe,index) => {
+            return <Recipe key = {index} title = {recipe.title} description = {recipe.description} />
         });
 
-        return (
+
+        }
+                return (
             <div className={classes.Recipes}>
                 {recipesToJSX}
 
