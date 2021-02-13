ccdb35123b333a692cdd2812d889b5aa56b251b0
AndresOverflow <andresoverflow@gmail.com>
2021-02-13 13:42:04 +0100

    recipe with medium css

--- src/containers/Recipes/Recipe/Recipe.js
+++ src/containers/Recipes/Recipe/Recipe.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import classes from './Recipe.css';
 
 import french_toasts_image from '../../../assets/images/french_toasts.jpg';
+import hour_glass from '../../../assets/svg/hour-glass.svg';
+import star_full from '../../../assets/svg/star-full.svg';
 
 import {Link} from 'react-router-dom';
 
@@ -21,9 +23,22 @@ class Recipe extends Component {
                 <Link to={url_link}>
                     <div className = {classes.RecipeId}>
                         <h2 className = {classes.Recipe_Name}> {this.props.title} </h2>
-                        <p className = {classes.Recipe_Image_Temp}> Image :D </p>
-                        <img src={french_toasts_image} alt="french_toast_image" className={classes.Recipe_Image}/>
-                        <p className = {classes.Recipe_Description}> {this.props.description}</p>
+                        <figure className={classes.Recipe_Figure}>
+                            <img src={french_toasts_image} alt="french_toast_image" className={classes.Recipe_Image}/>
+                        </figure>
+                        <h3 className = {classes.Recipe_Description}> {this.props.description}</h3>
+                        <div className={classes.Recipe_Characteristics}>
+                            <div className={classes.Recipe_Time}>
+                                <img src={hour_glass} alt="hour glass" className={classes.Recipe_Time_SVG}/>
+                                <p className={classes.Recipe_Time_Text}> 4.5 hours</p>
+                            </div>
+                            <div className={classes.Recipe_Star}>
+                                <img src={star_full} alt="star full" className={classes.Recipe_Star_SVG}/>
+                                <p className={classes.Recipe_Star_Text}> 4</p>
+                            </div>
+                        </div>
+
+                        
                     </div>
                 </Link>
 
@@ -33,3 +48,18 @@ class Recipe extends Component {
 }
 
 export default Recipe;
+
+/*
+<div className={classes.Recipe_Characteristics}>
+                            <div className={classes.Recipe_Time}>
+                                <img src={hour_glass} alt="hour glass" className={classes.Recipe_Time_SVG}/>
+                                <p className={classes.Recipe_Time_Text}> 4.5 hours</p>
+                            </div>
+                            <div className={classes.Recipe_Star}>
+                                <img src={star_full} alt="star full" className={classes.Recipe_Star_SVG}/>
+                                <p className={classes.Recipe_Star_Text}> 4</p>
+                            </div>
+
+                        </div>
+
+*/
